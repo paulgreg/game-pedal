1404cfd0516a0b20b9264c9b77feb554fe406a7b
Grégory Paul <me@gpaul.me>
2024-10-16 20:40:26 +0200

    chore: use Joystick library

--- game-pedal.ino
+++ game-pedal.ino
@@ -1,8 +1,15 @@
 // https://www.youtube.com/watch?v=xoqoUR1JBr8
 // https://www.partsnotincluded.com/how-to-emulate-an-xbox-controller-with-arduino-xinput/
 // https://www.reddit.com/r/arduino/comments/1b9cq62/using_arduino_micro_as_a_game_controller_confused/
+// https://github.com/MHeironimus/ArduinoJoystickLibrary
 
-#include <XInput.h>
+
+#include <Joystick.h>
+
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
+  JOYSTICK_TYPE_MULTI_AXIS, 4, 0,
+  false, false, false, false, false, false,
+  false, false, true, true, true);
 
 int analogPin = A5;
 
@@ -10,12 +17,14 @@ int analogPin = A5;
 
 void setup() {
   Serial.begin(115200);
-	XInput.begin();
-  XInput.setTriggerRange(0, 512);
+	Joystick.begin();
+  Joystick.setAcceleratorRange(0, 512);
 }
 void loop() {
   int value = analogRead(analogPin);
   if (DEBUG) Serial.println(value);
-  XInput.setTrigger(TRIGGER_RIGHT, value);
+  Joystick.setAccelerator(value);
   delay(10);
 }
+
+
